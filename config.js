3fcd8e45b6d7da5b15be5d854851f0c18c6b48c6
vilagen <vilagen@yahoo.com>
2019-12-20 17:29:02 -0500

    Got rid of bcrypt-nodejs, replaced with bcryptjs.

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-module.export = {
-  secert: 'thisIsASecretToEveryone'
+module.exports = {
+  secret: 'thisIsASecretToEveryone'
 };
--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const bcrypt = require("bcrypt-nodejs");
+const bcrypt = require("bcryptjs");
 
 // Define our model
 const userSchema = new Schema ({
@@ -24,9 +24,9 @@ userSchema.pre('save', function(next) {
         if(err) { return next(err); }
 
         // hash (encrypt) our password using this salt
-        bcrypt.hash(user.password, salt, null, function(err, hash) {
+        bcrypt.hash(user.password, salt, function(err, hash) {
             if (err) { return next(err); }
-
+            
             // overwrite plain text password with encrypted password
             user.password = hash;
             next();
@@ -35,7 +35,7 @@ userSchema.pre('save', function(next) {
 });
 
 userSchema.methods.comparePassword = function(candidatePassword, callback) {
-	bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+	bcrypt.compareSync(candidatePassword, this.password, function(err, isMatch) {
 		if (err) { return callback(err); }
 
 		callback(null, isMatch);
