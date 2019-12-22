121ff6d7e3551f432d647c56ef341a95db107a8e
vilagen <vilagen@yahoo.com>
2019-12-21 23:32:09 -0500

    Changed compareSync to Compare since using bcryptjs instead of brcypt-nodejs.

--- models/User.js
+++ models/User.js
@@ -35,7 +35,7 @@ userSchema.pre('save', function(next) {
 });
 
 userSchema.methods.comparePassword = function(candidatePassword, callback) {
-	bcrypt.compareSync(candidatePassword, this.password, function(err, isMatch) {
+	bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
 		if (err) { return callback(err); }
 
 		callback(null, isMatch);
